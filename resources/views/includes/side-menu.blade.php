f34ea2f90dfe95fa0ba7a22a3ff1f055c1c736e4
Hendrick <hendrickwijaya21@gmail.com>
2021-03-08 22:10:48 +0700

    Setup basic template layouts

---
+++ resources/views/includes/side-menu.blade.php
@@ -0,0 +1,38 @@
+<div class="welcome-menu">
+  <div class="logo">
+    <img src="https://cdn.pixabay.com/photo/2017/09/23/21/21/label-2780146_1280.png"/>
+  </div>
+  <div class="menu">
+    <div class="menu-title">
+      Menu
+    </div>
+    <ul class="links">
+      <li>
+        <a href="/menu">Menu</a>
+      </li>
+      <li>
+        <a href="/waitlist">Waitlist</a>
+      </li>
+      <li>
+        <a href="/giftcards">Giftcards</a>
+      </li>
+      <li>
+        <a href="/offers">Offers</a>
+      </li>
+    </ul> 
+  </div>
+  <div class="social-icons">
+    <a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+  </div>
+  <div class="location">
+    <div class="address">
+      234 Bank Street, <br>
+      New York, 11747
+    </div>
+    <div class="phone-number">
+      <a href="tel:718219863">718-219-863</a>
+    </div>
+  </div>
+</div>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="welcome-jumbo">
+    <div class="status">NEW</div>
+    <h1>Billy Burger</h1>
+    <img src="https://freepngimg.com/thumb/sandwich/74861-king-hamburger-food-ribs-fries-fast-burger.png"
+    class="burger-fries"
+    />
+  </div>
+@endsection
